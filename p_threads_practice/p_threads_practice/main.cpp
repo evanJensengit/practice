353269845c92183919f1b657f074f92ebe2784e2
Evan Jensen <jensen55@uw.edu>
2021-02-25 00:47:57 -0800

    p_threads practice

---
+++ p_threads_practice/p_threads_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p_threads_practice
+//
+//  Created by Evan Jensen on 2/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
